a271e1e060c09fc542289be783b8e65861550c0e
srotoswini <srotoswini.dev@gmail.com>
2022-06-10 13:26:46 +0530

    .

---
+++ new/_document.js
@@ -0,0 +1,65 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head/>
+        <link rel="manifest" href="site.webmanifest" />
+        <link
+          rel="shortcut icon"
+          type="image/x-icon"
+          href="assets/img/favicon.png"
+        />
+
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="assets/css/owl.carousel.min.css" />
+        <link rel="stylesheet" href="assets/css/animate.css" />
+        <link rel="stylesheet" href="assets/css/magnific-popup.css" />
+        <link rel="stylesheet" href="assets/css/all.min.css" />
+        <link rel="stylesheet" href="assets/css/flaticon.css" />
+        <link rel="stylesheet" href="assets/css/font.css" />
+        <link rel="stylesheet" href="assets/css/metisMenu.css" />
+        <link rel="stylesheet" href="assets/css/nice-select.css" />
+        <link rel="stylesheet" href="assets/css/slick.css" />
+        <link rel="stylesheet" href="assets/css/spacing.css" />
+        <link rel="stylesheet" href="assets/css/main.css" />
+        <link href="https://fonts.gstatic.com" rel="preconnect"/>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet"/>
+
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet"/>
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet"/>
+    <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet"/>
+    <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet"/>
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet"/>
+    <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet"/>
+  
+
+    <link href="assets/css/style.css" rel="stylesheet"/>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+      <script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
+      <script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
+      <script src="assets/js/popper.min.js"></script>
+      <script src="assets/js/bootstrap.min.js"></script>
+      <script src="assets/js/owl.carousel.min.js"></script>
+      <script src="assets/js/isotope.pkgd.min.js"></script>
+      <script src="assets/js/slick.min.js"></script>
+      <script src="assets/js/metisMenu.min.js"></script>
+      <script src="assets/js/jquery.nice-select.js"></script>
+      <script src="assets/js/ajax-form.js"></script>
+      <script src="assets/js/wow.min.js"></script>
+      <script src="assets/js/jquery.counterup.min.js"></script>
+      <script src="assets/js/waypoints.min.js"></script>
+      <script src="assets/js/jquery.scrollUp.min.js"></script>
+      <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+      <script src="assets/js/jquery.magnific-popup.min.js"></script>
+      <script src="assets/js/jquery.easypiechart.js"></script>
+      <script src="assets/js/plugins.js"></script>
+      <script src="assets/js/main.js"></script>
+    </Html>
+  );
+}
